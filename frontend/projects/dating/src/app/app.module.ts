af6b4b1569e6c9d75e2b58ea82df3460a39136c1
oleh.sarnovskyi <oleg.sarnovskiy@gmail.com>
2020-08-01 18:59:50 +0300

    Deleted shared modules.

--- frontend/projects/dating/src/app/app.module.ts
+++ frontend/projects/dating/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+import {LanguageTranslationModule} from "../../../../modules/shared/language-translation/language-translation.module";
+import {ScrollbarModule} from "../../../../modules/components/scrollbar/scrollbar.module";
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    LanguageTranslationModule,
+    ScrollbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
