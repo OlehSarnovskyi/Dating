b73e44e78572cd33bfe87d5e124ac4984ff32c9a
oleh.sarnovskyi <oleg.sarnovskiy@gmail.com>
2020-08-01 19:19:46 +0300

    Configured paths.

--- frontend/projects/dating/src/app/app.module.ts
+++ frontend/projects/dating/src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
-import {LanguageTranslationModule} from "../../../../modules/shared/language-translation/language-translation.module";
-import {ScrollbarModule} from "../../../../modules/components/scrollbar/scrollbar.module";
+import {LanguageTranslationModule} from "modules/shared/language-translation/language-translation.module";
+import {ScrollbarModule} from "modules/components/scrollbar/scrollbar.module";
 
 @NgModule({
   declarations: [
