787e59b279be71dd6f73bd8568e6fa57cd298a9d
oleh.sarnovskyi <oleg.sarnovskiy@gmail.com>
2020-08-07 11:43:48 +0300

    Added NGRX to dating project

--- frontend/projects/dating/src/app/app.module.ts
+++ frontend/projects/dating/src/app/app.module.ts
@@ -1,12 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
 import {LanguageTranslationModule} from "modules/shared/language-translation/language-translation.module";
 import {ScrollbarModule} from "modules/components/scrollbar/scrollbar.module";
+import {StoreModule} from "@ngrx/store";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {environment} from "../environments/environment";
+import {DEFAULT_ROUTER_FEATURENAME, routerReducer, StoreRouterConnectingModule} from "@ngrx/router-store";
+
+
+const NGRX_MODULES = [
+  StoreModule.forRoot({
+    [DEFAULT_ROUTER_FEATURENAME]: routerReducer
+  }, {}),
+  StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
+  StoreRouterConnectingModule.forRoot()
+]
 
 @NgModule({
   declarations: [
@@ -17,6 +30,7 @@ import {ScrollbarModule} from "modules/components/scrollbar/scrollbar.module";
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    ...NGRX_MODULES,
     LanguageTranslationModule,
     ScrollbarModule
   ],
