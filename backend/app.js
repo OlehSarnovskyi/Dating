ad57da82018d21389038f27252a6e9a88870d307
oleh.sarnovskyi <oleg.sarnovskiy@gmail.com>
2020-08-10 18:04:43 +0300

    Connected to MongoDB. Created user schema.

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
 const authRoutes = require('./routes/auth')
+const keys = require('./config/keys')
 
 const app = express()
+
+
+mongoose.connect(keys.MONGO_URI)
+    .then(() => console.log('MongoDB connected.'))
+    .catch(error => console.log(error))
 
 
 app.use(morgan('dev'))
---
+++ backend/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI: 'mongodb+srv://olehSarnovskyi:iuwYdffv10NFwMwR@cluster0.9gdpi.mongodb.net/Dating?retryWrites=true&w=majority'
+}
