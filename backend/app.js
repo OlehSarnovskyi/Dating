2d1cca6f28d46b336d567c112ad935115b106ef5
oleh.sarnovskyi <oleg.sarnovskiy@gmail.com>
2020-08-10 16:19:34 +0300

    Configured starting the server, cors, encoded, logs. Created controllers and routes for auth.

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const authRoutes = require('./routes/auth')
+
+const app = express()
+
+
+app.use(morgan('dev'))
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.use('/api/auth', authRoutes)
+
+module.exports = app
