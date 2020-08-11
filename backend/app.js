d0e8d8c3501425a621a819d940029c3a70120b9d
oleh.sarnovskyi <oleg.sarnovskiy@gmail.com>
2020-08-11 17:45:39 +0300

    Created middleware passport.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const passport = require('passport')
 const cors = require('cors')
 const morgan = require('morgan')
 
@@ -15,6 +16,8 @@ mongoose.connect(keys.MONGO_URI)
     .catch(error => console.log(error))
 
 
+app.use(passport.initialize())
+require('./middleware/passport')(passport)
 app.use(morgan('dev'))
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
