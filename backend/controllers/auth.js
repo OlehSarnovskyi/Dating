97cf28e00e9c14901cf2adddf0c28906e6a5abe5
oleh.sarnovskyi <oleg.sarnovskiy@gmail.com>
2020-08-11 10:05:32 +0300

    Created register controller.

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,10 +1,36 @@
-module.exports.register = (req, res) => {
-    res.status(200).json({
-        register: {
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/User')
+
+
+module.exports.register = async (req, res) => {
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        // if user with this mail already registered
+        res.status(409).json({
+            shortCode: 'USER_WITH_MAIL_ALREADY_EXIST'
+        })
+    } else {
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+
+        const user = new User({
             email: req.body.email,
-            password: req.body.password
+            password: bcrypt.hashSync(password, salt)
+        })
+
+        try {
+            await user.save()
+            res.status(201).json({
+                user,
+                shortCode: 'USER_CREATED'
+            })
+        } catch (e) {
+
         }
-    })
+
+    }
 }
 
 module.exports.login = (req, res) => {
