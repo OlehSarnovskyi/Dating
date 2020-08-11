b6788351752edb383ad3bcb3a518d93bb781f89e
oleh.sarnovskyi <oleg.sarnovskiy@gmail.com>
2020-08-11 11:22:51 +0300

    Created login controller.

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGO_URI: 'mongodb+srv://olehSarnovskyi:iuwYdffv10NFwMwR@cluster0.9gdpi.mongodb.net/Dating?retryWrites=true&w=majority'
+    MONGO_URI: 'mongodb+srv://olehSarnovskyi:iuwYdffv10NFwMwR@cluster0.9gdpi.mongodb.net/Dating?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
 }
--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,15 +1,17 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/User')
+const keys = require('../config/keys')
 
 
 module.exports.register = async (req, res) => {
     const candidate = await User.findOne({email: req.body.email})
 
     if (candidate) {
-        // if user with this mail already registered
+        // if user with this email already registered
         res.status(409).json({
-            shortCode: 'USER_WITH_MAIL_ALREADY_EXIST'
+            shortCode: 'USER_WITH_EMAIL_ALREADY_EXIST'
         })
     } else {
         const salt = bcrypt.genSaltSync(10)
@@ -33,8 +35,33 @@ module.exports.register = async (req, res) => {
     }
 }
 
-module.exports.login = (req, res) => {
-    res.status(200).json({
-        login: true
-    })
+module.exports.login = async (req, res) => {
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+
+        if (passwordResult) {
+            // gen token, passwords match
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60 * 60})
+
+            res.status(200).json({
+                token: `Bearer ${token}`,
+                shortCode: 'SIGNED_IN'
+            })
+        } else {
+            res.status(401).json({
+                shortCode: 'INCORRECT_PASSWORD'
+            })
+        }
+
+    } else {
+        // if user with this email not found
+        res.status(404).json({
+            shortCode: 'USER_WITH_EMAIL_NOT_FOUND'
+        })
+    }
 }
