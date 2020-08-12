7634cd433ff9839f1f005f800843ce48a293f617
oleh.sarnovskyi <oleg.sarnovskiy@gmail.com>
2020-08-12 22:58:51 +0300

    Configured auth layout.

--- frontend/projects/dating/src/app/layouts/auth-layout/auth-layout.component.ts
+++ frontend/projects/dating/src/app/layouts/auth-layout/auth-layout.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-auth-layout',
-  templateUrl: './auth-layout.component.html',
+  template: `
+    <div class="container">
+      <router-outlet></router-outlet>
+    </div>
+  `,
   styleUrls: ['./auth-layout.component.scss']
 })
 export class AuthLayoutComponent implements OnInit {
