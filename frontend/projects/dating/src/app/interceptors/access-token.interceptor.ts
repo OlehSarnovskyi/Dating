18efc4f9f68767689d624cd9254d01cadf27941b
oleh.sarnovskyi <oleh.sarnovskyi@etiya.com>
2020-09-01 12:33:38 +0300

    Rewrite interceptor.

--- frontend/projects/dating/src/app/interceptors/access-token.interceptor.ts
+++ frontend/projects/dating/src/app/interceptors/access-token.interceptor.ts
@@ -1,25 +1,35 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {select, Store} from "@ngrx/store";
-import {getAccessToken} from "../layouts/auth-layout/pages/login";
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {EMPTY, Observable} from 'rxjs';
+import {select, Store} from '@ngrx/store';
+import {getAccessToken} from '../layouts/auth-layout/pages/login';
+import {catchError, filter, first} from 'rxjs/operators';
+import {flatMap} from 'rxjs/internal/operators';
 
 @Injectable()
 export class AccessTokenInterceptor implements HttpInterceptor {
 
   constructor(private store$: Store) {}
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.store$.pipe(select(getAccessToken)).subscribe((accessToken: string) => {
-      if (accessToken) {
-        request = request.clone({
-          setHeaders: {
-            Authorization: accessToken
-          }
-        })
-      }
-      return next.handle(request)
-    })
-    return next.handle(request)
+  intercept(request: HttpRequest<any>, next: HttpHandler): (Observable<HttpEvent<any>> | any) {
+    this.store$.pipe(
+      select(getAccessToken),
+      first(),
+      filter(accessToken => !!accessToken),
+      flatMap((accessToken: string) => {
+        const authRequest = request.clone({
+            setHeaders: {
+              Authorization: accessToken
+            }
+          })
+        return next.handle(authRequest).pipe(
+          filter(err => err instanceof HttpErrorResponse && err.status === 401),
+          catchError(err => {
+            console.log(err)
+            return EMPTY
+          })
+        )
+      })
+    )
   }
 }
