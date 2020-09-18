6b0967f52e4902fc084585cb9a956c5286a85a2e
oleh.sarnovskyi <oleg.sarnovskiy@gmail.com>
2020-09-18 11:52:20 +0300

    Create authSchema and changed userSchema.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose')
+const authSchema = require('./Auth')
 
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+    authData: authSchema,
 })
 
 module.exports = mongoose.model('users', userSchema)
